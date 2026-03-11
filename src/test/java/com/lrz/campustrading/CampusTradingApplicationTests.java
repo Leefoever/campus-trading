421bafdd2e007aa66adf5ec83043c146abfadb40
Leefoever <2587973885@qq.com>
2026-03-11 13:26:15 +0800

    init project

---
+++ src/test/java/com/lrz/campustrading/CampusTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrz.campustrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
